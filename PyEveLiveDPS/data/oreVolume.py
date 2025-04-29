b21a85daf051abcd16291988b4f0c8127cb886ad
blazemonger <paulvdheu@gmail.com>
2025-04-29 17:52:32 +0100

    Update oreVolume.py

--- PyEveLiveDPS/data/oreVolume.py
+++ PyEveLiveDPS/data/oreVolume.py
@@ -1,9 +1,9 @@
 _oreVolume = {
-    "Carbonaceous Ore":1,
-    "Deep-Core Carbon Ore":1,
-    "Common Ore":1,
-    "Metal-Rich Ore":1,
-    "2Deep-Core Metallic Ore":1,
-    "Aestasium":1,
-    "Hermetite":1
+    "Carbonaceous Ore": 0.1,
+    "Deep-Core Carbon Ore": 0.1,
+    "Common Ore": 0.1,
+    "Metal-Rich Ore": 0.1,
+    "Deep-Core Metallic Ore": 0.1,
+    "Aestasium": 0.1,
+    "Hermetite": 0.1
 }
